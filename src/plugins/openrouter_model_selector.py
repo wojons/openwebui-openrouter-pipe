035e49adda27441e380bc4410e5fb0d165fa2802
Alexis Okuwa <wojonstech@gmail.com>
2025-03-31 03:30:46 -0500

    update version number

--- src/plugins/openrouter_model_selector.py
+++ src/plugins/openrouter_model_selector.py
@@ -1,7 +1,7 @@
 """
 title: OpenRouter Model Selector
 author: Cline
-version: 0.1.1
+version: 0.1.2
 license: MIT
 description: Integrates OpenRouter model selection into OpenWebUI with a FREE_ONLY filter option
 """
